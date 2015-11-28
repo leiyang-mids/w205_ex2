f2c2c5687e1947a7c79873f8f26dca00a137e85b
LEI YANG <lei.yng@gmail.com>
2015-11-28 12:50:30 -0500

    add "drop if exists"

--- setup/create_db.py
+++ setup/create_db.py
@@ -5,21 +5,18 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 conn = psycopg2.connect(database="postgres", user="postgres", password="pass", host="localhost", port="5432")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = conn.cursor()
-# TODO: add cleanup step - drop first if exist
-cur.execute('drop database tcount')
+# cleanup step - drop first if exists
+cur.execute('DROP DATABASE IF EXISTS tcount')
 conn.commit()
 # create anew
-cur.execute("create database tcount")
+cur.execute("CREATE DATABASE tcount")
 conn.commit()
 cur.close()
 conn.close()
 print 'database tcount is successfully created!'
 
-# create table in Tcount
-# connect to database and create table & column
+# connect to tcount database and create table & column
 conn = psycopg2.connect(database="tcount", user="postgres", password="pass", host="localhost", port="5432")
-
-#Create tweetwordcount table
 cur = conn.cursor()
 cur.execute('''CREATE TABLE Tweetwordcount
        (word TEXT PRIMARY KEY     NOT NULL,
@@ -27,3 +24,4 @@ cur.execute('''CREATE TABLE Tweetwordcount
 conn.commit()
 conn.close()
 print 'table tweetwordcount is successfully created!'
+print 'setup completed, you are ready to go, happy streaming :)'
